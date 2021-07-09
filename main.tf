2915856adfcaf225d6d658a496d70f247463a80a
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-07-08 23:56:47 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -65,8 +65,8 @@ resource "aws_route53_record" "wildcard_validation" {
   name            = aws_acm_certificate.wildcard_website.domain_validation_options.*.resource_record_name[0]
   records         = [aws_acm_certificate.wildcard_website.domain_validation_options.*.resource_record_value[0]]
   type            = aws_acm_certificate.wildcard_website.domain_validation_options.*.resource_record_type[0]
-  zone_id         = aws_route53_zone.wildcard_website.zone_id
-  #zone_id = "${aws_route53_zone.main.zone_id}"
+  #zone_id         = aws_route53_zone.wildcard_website.zone_id
+  zone_id = "${aws_route53_zone.main.zone_id}"
   allow_overwrite = true
   ttl             = 60
 }  
