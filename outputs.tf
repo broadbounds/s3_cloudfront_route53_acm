dae8f36c7ef5627b7b6d0aa9d416a54c830a9028
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-07-08 18:58:56 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "testing" {
-  value = "Test this demo code by going to https://${aws_route53_record.myapp.fqdn} and checking your have a valid SSL cert"
+  value = "Test this demo code by going to https://${aws_route53_record.www.fqdn} and checking your have a valid SSL cert"
 }
 output "testing_sclient" {
-  value = "Test this SSL by using openssl s_client -host ${aws_route53_record.myapp.fqdn} -port 443 and looking at the certs"
+  value = "Test this SSL by using openssl s_client -host ${aws_route53_record.www.fqdn} -port 443 and looking at the certs"
 }
 
 output "s3_bucket_details" {
-  value       = aws_s3_bucket.www
+  value       = aws_s3_bucket.b
   description = "The details of the S3 bucket"
 }
 
 output "aws_cloudfront_details" {
-  value       = aws_cloudfront_distribution.www_distribution
+  value       = aws_cloudfront_distribution.s3_distribution
   description = "The details of the cloudfront"
 }
 
